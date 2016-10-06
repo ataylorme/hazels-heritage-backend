cf32c82a7631b924a8f459a6411ac5e5db4e9a33
ataylorme <andrew@ataylor.me>
2016-10-06 17:24:06 -0500

    Add ataylorme-theme

---
+++ web/wp-content/themes/ataylorme/functions.php
@@ -0,0 +1,24 @@
+<?php
+namespace ataylorme;
+
+/**
+ * This is a decoupled app
+ * there is no WordPress front-end
+ * so we'll redirect all traffic to the admin
+ */
+function redirect_to_admin() {
+
+	if ( ! is_admin() ) {
+		wp_redirect( get_admin_url() );
+		exit();
+	}
+
+}
+
+add_action( 'template_redirect', __NAMESPACE__ . '\redirect_to_admin', 99 );
+
+function setup_theme() {
+	add_theme_support( 'post-thumbnails' );
+}
+
+add_action( 'after_setup_theme', __NAMESPACE__ . '\setup_theme' );
